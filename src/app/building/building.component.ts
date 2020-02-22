ab559758f74b8da88e731f1ffe3590c315dcf6ef
OFIRI <ofir937@gmail.com>
2020-02-22 20:48:45 +0200

    add list of floors

--- src/app/building/building.component.ts
+++ src/app/building/building.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Floor {
+  index: number;
+  img: string;
+}
+
 @Component({
   selector: 'building',
   templateUrl: './building.component.html',
   styleUrls: ['./building.component.scss']
 })
 export class BuildingComponent implements OnInit {
-  numOfFloors: Number = 0;
+  floors: Floor[] = [];
+  floorImg = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateNumOfFloors(formData: any) {
+    debugger;
+    if (isNaN(Number(formData.numOfFloors))) {
+      alert('מספר קומות לא תקין');
+      return;
+    }
+    for(let i=1;i<=Number(formData.numOfFloors);i++) {
+      let newFloor = {index:i,img:this.floorImg};
+      this.floors.push(newFloor);
+    }
   }
 
 }
