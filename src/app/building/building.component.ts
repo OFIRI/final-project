4062630c1199961af0f67e08b31ad56bd98cb15c
OFIRI <ofir937@gmail.com>
2020-02-23 07:47:25 +0200

    add building view

--- src/app/building/building.component.ts
+++ src/app/building/building.component.ts
@@ -12,7 +12,6 @@ export interface Floor {
 })
 export class BuildingComponent implements OnInit {
   floors: Floor[] = [];
-  floorImg = "";
 
   constructor() { }
 
@@ -20,13 +19,25 @@ export class BuildingComponent implements OnInit {
   }
 
   updateNumOfFloors(formData: any) {
-    debugger;
     if (isNaN(Number(formData.numOfFloors))) {
       alert('מספר קומות לא תקין');
       return;
     }
+    this.floors = [];
     for(let i=1;i<=Number(formData.numOfFloors);i++) {
-      let newFloor = {index:i,img:this.floorImg};
+      console.log(i);
+
+      let newFloor:Floor;
+      let regularfloorImg = "./../assets/imgs/regular.png";
+      let firstfloorImg = "./../assets/imgs/first.png";
+      let lastfloorImg = "./../assets/imgs/last.png";
+      if(i == 1) {
+        newFloor = {index:i,img:firstfloorImg};
+      } else if(i==Number(formData.numOfFloors)) {
+        newFloor = {index:i,img:lastfloorImg};
+      } else {
+        newFloor = {index:i,img:regularfloorImg};
+      }
       this.floors.push(newFloor);
     }
   }
